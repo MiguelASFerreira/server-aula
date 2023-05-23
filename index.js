407b7ff813f7d97b77448f53a159c082ca4e9d20
Miguel <migant1306@gmail.com>
2023-05-22 22:46:47 -0300

    update

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-const express = require("express");
-const app = express();
+const { server } = require("./server")
 
-
+const port = 3000
+server.listen(port, () => {
+    console.log(`Server is running on port ${port}`)
+})
---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const productsRoutes = require("./routes/products")
+const usersRoutes = require("./routes/users")
+
+const server = express();
+server.use(express.json());
+
+server.use(productsRoutes.router)
+server.use(usersRoutes.router)
+
+
+module.exports = {
+    server
+}
